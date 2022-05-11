2ec3bfd4c6aeb680db40d4c151ea4a532e44b6c2
swaglagmagrag <75270704+swaglagmagrag@users.noreply.github.com>
2022-05-11 08:21:52 -0500

    Update Hexi.lua

--- Hexi.lua
+++ Hexi.lua
@@ -1,28 +1,32 @@
-local a = {255, 255, 255}
-local b = "ABCDEF"
-local c = {{}, {}, {}}
-local d = {}
-local hex = ""
-for k, v in ipairs(a) do
-  local d = v / 16
-  local e = (d - math.floor(d))
-  local f = e * 16
-  local g = math.floor(d)
-  local h = g / 16
-  local i = h * 16
-  table.insert(c[k], i)
-  table.insert(c[k], f)
-end
+function get_hex(num)
+  local b = "ABCDEF"
+  local c = {{}, {}, {}}
+  local d = {}
+  local hex = ""
+  for k, v in ipairs(num) do
+    local d = v / 16
+    local e = (d - math.floor(d))
+    local f = e * 16
+    local g = math.floor(d)
+    local h = g / 16
+    local i = h * 16
+    table.insert(c[k], i)
+    table.insert(c[k], f)
+  end
 
-for k, v in ipairs(c) do
-  for i = 1, #v do
-    if v[i] > 9 then
-      local j = v[i] - 9
-      local k = b:sub(j,j)
-      hex = hex .. k
-    else
-      hex = hex .. tostring(math.floor(v[i]))
+  for k, v in ipairs(c) do
+    for i = 1, #v do
+      if v[i] > 9 then
+        local j = v[i] - 9
+        local k = b:sub(j,j)
+        hex = hex .. k
+      else
+        hex = hex .. tostring(math.floor(v[i]))
+      end
     end
   end
+  return hex
 end
-print(hex)
+
+local b = {math.random(1, 255), math.random(1, 255), math.random(1, 255)}
+print(get_hex(b))
